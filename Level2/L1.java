d80073a583c29a5715f0ca7208af46afdc015252
AmitATK <97805421+AmitATK@users.noreply.github.com>
2026-07-14 18:28:32 +0000

    l2started

--- Level2/L1.java
+++ Level2/L1.java
@@ -1,8 +1,15 @@
 package Level2;
 //11. Write a program to read a number and check whether it is even or odd.
 
+import java.util.Scanner;
+
 public class L1 {
     public static void main(String[] args) {
-        
+        System.out.println("Type a number to check even or odd");
+        Scanner num1 = new Scanner(System.in);
+
+        int number=num1.nextInt();
+        if(number %2 ==0) System.out.println("Even number");
+        else System.out.println("Odd number");
     }
 }
---
+++ Level2/L2.java
@@ -0,0 +1,8 @@
+package Level2;
+//12. Write a program to read a number and check whether it is positive, negative or zero.
+
+public class L2 {
+    public static void main(String[] args) {
+        System.out.println("Type a number to check it is positive or nogatice");
+    }
+}
